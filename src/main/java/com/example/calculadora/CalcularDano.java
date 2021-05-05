8d53cea33b4df6157da14f35e13eeaf1178bd7d8
jesusgm1 <72188507+jesusgm1@users.noreply.github.com>
2021-05-05 10:37:06 +0200

    Add files via upload

---
+++ src/main/java/com/example/calculadora/CalcularDano.java
@@ -0,0 +1,20 @@
+package com.example.calculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalcularDano extends AppCompatActivity {
+int calculo = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calcular_dano);
+    }
+
+
+
+
+
+
+}
